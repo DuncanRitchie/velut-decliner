f51de346b708b9c12d4c0b1a316c61244d8df7d4
Duncan Ritchie <duncan.ritchie@informationcatalyst.com>
2021-01-03 18:36:05 +0000

    Declension schemata in JS

--- main.js
+++ main.js
@@ -1,33 +1,64 @@
-const charsElement = document.getElementById("characters-phonetic");
+const buttonDecline = document.getElementById("decline");
 
-const phoneticData = `
-Replace this text with the text you want to get the distinct characters from.
-`;
-
-const removeDuplicateChars = (string) => {
-    let charsObject = {};
-    let charsString = "";
-    for (let i = 0; i < string.length; i++) {
-        const currentChar = string[i];
-        if (!charsObject[currentChar]) {
-            charsString += currentChar;
-            charsObject[currentChar] = true;
-        }
-    }
-    return charsString;
-};
-
-const sortString = (string) => {
-    let charsArray = [];
-    for (let i = 0; i < string.length; i++) {
-        charsArray.push(string[i]);
-    }
-    charsArray.sort();
-    return charsArray.join("");
-};
-
-const updateDomElement = () => {
-    charsElement.textContent = sortString(removeDuplicateChars(phoneticData));
-}
-
-updateDomElement();
+const schemata = [
+    {
+        "Description":        "1st, noun, Latin-style",
+        "Unstressed endings": ["a","ae","am","ā","ās","īs"],
+        "Stressed endings":   ["ārum"],
+    },
+    {
+        "Description":        "1st, noun, Greek-style",
+        "Unstressed endings": ["ē","ēs","ēn","ae"],
+        "Stressed endings":   ["ārum"],
+    },
+    {
+        "Description":        "2nd, noun, masculine/feminine",
+        "Unstressed endings": ["us","ī","e","ō","um","ōs","īs"],
+        "Stressed endings":   ["ōrum"],
+    },
+    {
+        "Description":        "2nd, noun, neuter",
+        "Unstressed endings": ["um","ī","ō","um","a","īs"],
+        "Stressed endings":   ["ōrum"],
+    },
+    {
+        "Description":        "3rd, noun, -i stem",
+        "Unstressed endings": ["","is"],
+        "Stressed endings":   [],
+    },
+    {
+        "Description":        "3rd, noun, non-i stem",
+        "Unstressed endings": ["","is"],
+        "Stressed endings":   [],
+    },
+    {
+        "Description":        "4th, noun, -us nominative",
+        "Unstressed endings": ["ūs"],
+        "Stressed endings":   [],
+    },
+    {
+        "Description":        "4th, noun, -ū nominative",
+        "Unstressed endings": ["ūs"],
+        "Stressed endings":   [],
+    },
+    {
+        "Description":        "5th, noun",
+        "Unstressed endings": ["em","ē","ēs"],
+        "Stressed endings":   ["ēī","ērum","ēbus"],
+    },
+    {
+        "Description":        "1st/2nd, adjective",
+        "Unstressed endings": ["us","ī","e","ō","um","ōs","a","ae","am","ā","ās","īs","ior","ē","ius"],
+        "Stressed endings":   ["ārum","ōrum"],
+    },
+    {
+        "Description":        "3rd, adjective, one-form nominative singular",
+        "Unstressed endings": ["","is","em","ī","e","ēs","ia","um","ibus"],
+        "Stressed endings":   [],
+    },
+    {
+        "Description":        "3rd, adjective, two-form nominative singular",
+        "Unstressed endings": ["","e","is","em","ī","ēs","ia","um","ibus"],
+        "Stressed endings":   [],
+    },
+]
